a675558b62b704225bf860ef40037277b82b1117
Frank J. Reashore <frank.reashore@gov.bc.ca>
2017-12-14 16:23:22 -0800

    Initial commit

--- ClbcRestService/Controllers/ValuesController.cs
+++ ClbcRestService/Controllers/ValuesController.cs
@@ -24,21 +24,21 @@ namespace ClbcRestService.Controllers
         }
 
         // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
